2f741acb71dbbba053dab2e3801eec65b57d2d9e
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-03-29 22:09:09 +0900

    クライアントをコミット

---
+++ apis/coincheck/client/public.rb
@@ -0,0 +1,21 @@
+module Coincheck
+  module Client
+    class Public
+      URL = "https://coincheck.com"
+
+      def initialize
+        @conn = Faraday.new(url: URL, headers: { "Content-Type" => "application/json" }) do |config|
+          config.adapter :net_http_persistent
+        end
+      end
+
+      def rate(pair, **args)
+        @conn.get("/api/rate/#{pair}", args)
+      end
+
+      def trades(**args)
+        @conn.get("/api/trades", args)
+      end
+    end
+  end
+end
