33e8781687ae58ee9c07fa5b6e0a57d844d9ae51
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-03-29 09:31:39 +0900

    明示的に設定

--- lib/coincheck/fetch_trade.rb
+++ lib/coincheck/fetch_trade.rb
@@ -24,11 +24,16 @@ module Coincheck
 
     def format_data_item(item)
       time = Time.zone.parse(item["created_at"])
-      item.merge(
+      {
+        id: item.fetch("id"),
+        pair: item.fetch("pair"),
+        price: item.fetch("rate"),
+        amount: item.fetch("amount"),
+        side: item.fetch("order_type"),
         created_at_utc: time.utc,
         created_at_jst: time,
         timestamp: time.to_i
-      )
+      }
     end
   end
 end
