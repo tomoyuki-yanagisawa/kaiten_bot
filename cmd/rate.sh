06b5512611fe91e8b6f5c87d3538696e0c88ff33
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-03-31 22:06:30 +0900

    bash を追加

---
+++ cmd/rate.sh
@@ -0,0 +1,1 @@
+ruby cli/coincheck/fetch_rate_cli.rb btc_jpy | jq '.rate'
--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,6 @@
 set -e
 set -o pipefail
 
-ruby cli/coincheck/fetch_rate_cli.rb btc_jpy | jq
-ruby cli/coincheck/fetch_trade_list_cli.rb btc_jpy | jq '.list[0:2]'
-
 TERMINATE=true ruby bin/watch_coincheck_trades.rb
 
 ruby cli/coincheck/get_trade_list_cli.rb | jq '.[0:2]'
@@ -12,4 +9,6 @@ ruby cli/coincheck/get_candle_list_cli.rb 1 | jq '.[0:2]'
 ruby cli/coincheck/fetch_rate_cli.rb xxx_yyy | jq
 ruby cli/coincheck/fetch_trade_list_cli.rb xxx_yyy | jq
 
+bash `dirname $0`/../cmd/rate.sh
+bash `dirname $0`/../cmd/trade_list.sh
 bash `dirname $0`/../cmd/orderbook.sh
