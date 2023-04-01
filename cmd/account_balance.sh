4eb60f49e1c6903bd2f2b0123535646396f284cf
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-04-01 22:18:11 +0900

    Revert "delete pipe"
    This reverts commit ce31074ec883705d5291faeee4ff9a7243bf07de.

--- cmd/account_balance.sh
+++ cmd/account_balance.sh
@@ -3,4 +3,5 @@ set -o pipefail
 
 EXCHANGE=$1
 
-ruby script/${EXCHANGE}/fetch_account_balance_cli.rb
+ruby script/${EXCHANGE}/fetch_account_balance_cli.rb |
+  jq -e -r '.balance | to_entries[] | [.key, .value.total, .value.free, .value.locked] | @tsv'
