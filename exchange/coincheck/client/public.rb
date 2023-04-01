768834661f34eeaaa409079d15fa0dea4b3e2d04
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-04-01 17:32:12 +0900

    fix nest

--- exchange/coincheck/client/public.rb
+++ exchange/coincheck/client/public.rb
@@ -1,25 +1,23 @@
-module Coincheck
-  module Client
-    class Public
-      URL = "https://coincheck.com".freeze
+module Coincheck::Client
+  class Public
+    URL = "https://coincheck.com".freeze
 
-      def initialize
-        @conn = Faraday.new(url: URL, headers: { "Content-Type" => "application/json" }) do |config|
-          config.adapter :net_http_persistent
-        end
+    def initialize
+      @conn = Faraday.new(url: URL, headers: { "Content-Type" => "application/json" }) do |config|
+        config.adapter :net_http_persistent
       end
+    end
 
-      def rate(pair, **args)
-        @conn.get("/api/rate/#{pair}", args)
-      end
+    def rate(pair, **args)
+      @conn.get("/api/rate/#{pair}", args)
+    end
 
-      def trades(**args)
-        @conn.get("/api/trades", args)
-      end
+    def trades(**args)
+      @conn.get("/api/trades", args)
+    end
 
-      def order_books(**args)
-        @conn.get("/api/order_books", args)
-      end
+    def order_books(**args)
+      @conn.get("/api/order_books", args)
     end
   end
 end
