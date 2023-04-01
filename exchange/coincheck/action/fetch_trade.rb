971aa98b63eb5fa98607b564c535031d90a5e681
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-04-01 21:17:25 +0900

    Decimal で格納しておく

--- exchange/coincheck/action/fetch_trade.rb
+++ exchange/coincheck/action/fetch_trade.rb
@@ -28,8 +28,8 @@ module Coincheck::Action
       {
         id: item.fetch("id"),
         pair: item.fetch("pair"),
-        price: item.fetch("rate"),
-        amount: item.fetch("amount"),
+        price: item.fetch("rate").to_d,
+        amount: item.fetch("amount").to_d,
         side: item.fetch("order_type"),
         created_at_utc: time.utc,
         created_at_jst: time,
