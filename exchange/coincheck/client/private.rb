f3b0783b2e7a08fc2956e98dd122a91c2bf41ca7
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-04-01 18:26:18 +0900

    body を必須にする

--- exchange/coincheck/client/private.rb
+++ exchange/coincheck/client/private.rb
@@ -10,18 +10,18 @@ module Coincheck::Client
 
     def account_balance(**args)
       @conn.get("/api/accounts/balance", args) do |req|
-        req.headers.merge! calc_access_header(req.path)
+        req.headers.merge! calc_access_header(req.path, req.body)
       end
     end
 
     private
 
-    def calc_access_header(path, body = "")
+    def calc_access_header(path, body)
       apikey = ENV.fetch("_COINCHECK_APIKEY", nil)
       secret = ENV.fetch("_COINCHECK_SECRET", nil)
 
       nonce = Time.now.to_i.to_s
-      message = nonce + URI.join(URL, path).to_s + body
+      message = nonce + URI.join(URL, path).to_s + body.to_s
       signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), secret, message)
 
       {
