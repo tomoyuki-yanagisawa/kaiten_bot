ce31074ec883705d5291faeee4ff9a7243bf07de
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-04-01 22:06:53 +0900

    delete pipe

--- cmd/account_balance.sh
+++ cmd/account_balance.sh
@@ -3,5 +3,4 @@ set -o pipefail
 
 EXCHANGE=$1
 
-ruby script/${EXCHANGE}/fetch_account_balance_cli.rb |
-  jq -e -r '.balance | to_entries[] | [.key, .value.total, .value.free, .value.locked] | @tsv'
+ruby script/${EXCHANGE}/fetch_account_balance_cli.rb
