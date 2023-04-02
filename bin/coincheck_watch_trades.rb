978f097798da5d6a3752bd91c6865ded8a3a930c
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-04-03 01:47:12 +0900

    ARVG 読み込みは定数にした

--- bin/coincheck_watch_trades.rb
+++ bin/coincheck_watch_trades.rb
@@ -3,19 +3,19 @@ require_relative "../boot"
 $stdout.sync = true
 
 _terminate = ENV["TERMINATE"] || false
-pair = ARGV.fetch(0)
 
 Signal.trap(:INT) { _terminate = true }
 
+PAIR = ARGV.fetch(0)
 INTERVAL_CONST_MILI_SEC = 500
 INTERVAL_ERROR_MILI_SEC = 5
 
 client = Coincheck::Client::Public.new
 
-store = ExchangeTrade.build(exchange: :coincheck, pair:)
+store = ExchangeTrade.build(exchange: :coincheck, pair: PAIR)
 
 Helper.loop_duration(interval_const_msec: INTERVAL_CONST_MILI_SEC, interval_error_msec: INTERVAL_ERROR_MILI_SEC) do
-  trade = Coincheck::Action::FetchTrade.request(client, pair:)
+  trade = Coincheck::Action::FetchTrade.request(client, pair: PAIR)
 
   last_trade = trade.fetch(:list).first
 
