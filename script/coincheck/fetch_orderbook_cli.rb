94a75a458e56520179ee161747710b4284994d7a
tomoyuki-yanagisawa <yanagisawa.tomoyuki@gmail.com>
2023-04-03 22:23:22 +0900

    デフォルト値の書き方を修正

--- script/coincheck/fetch_orderbook_cli.rb
+++ script/coincheck/fetch_orderbook_cli.rb
@@ -1,4 +1,4 @@
 require_relative "../../boot"
 
 client = Coincheck::Client::Public.new
-puts Coincheck::Action::FetchOrderbook.request(client, pair: ARGV[0] || :btc_jpy, limit: ARGV[1] || 100).to_json
+puts Coincheck::Action::FetchOrderbook.request(client, pair: ARGV[0] || :btc_jpy, limit: ARGV[1]&.to_i || 100).to_json
--- script/coincheck/get_candle_list_cli.rb
+++ script/coincheck/get_candle_list_cli.rb
@@ -1,5 +1,5 @@
 require_relative "../../boot"
 
-unit_minutes = ARGV[1].try(:to_i) || 5
+unit_minutes = ARGV[1]&.to_i || 5
 
 puts ExchangeTrade.build(exchange: :coincheck, pair: ARGV.fetch(0)).get_candles(unit: unit_minutes * 60).to_json
